f3f9186645a160d7a32d3557b643efefeeb08ecb
RrePitso <80382152+RrePitso@users.noreply.github.com>
2026-06-17 13:41:46 +0200

    Include parent directories in sys.path
    Add sys.path manipulation to include parent directories.

--- app/pages/3_⚙️_Model_Status.py
+++ app/pages/3_⚙️_Model_Status.py
@@ -1,5 +1,8 @@
-import streamlit as st
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+import streamlit as st
 from src.config import MODEL_DIR
 import subprocess
 
